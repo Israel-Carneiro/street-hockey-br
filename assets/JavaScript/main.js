ab71682787837b830ca3f1438a5cb270957ab7f4
Israel-Carneiro <raelc.oliveira@gmail.com>
2023-01-20 20:37:22 -0300

    Apagando código em desuso

--- assets/JavaScript/main.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector ('.botao__menu')
-const menu = document.querySelector ('.menu__navegacao')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu__navegacao-ativo')
-})
